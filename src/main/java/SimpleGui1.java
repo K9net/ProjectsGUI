8a004cc825ee62d7aab18bec6c10d9a7157ba910
Valeriy V <valera.vasilev.2002@mail.ru>
2023-01-23 07:44:18 +0300

    First commit

--- src/main/java/SimpleGui1.java
+++ src/main/java/SimpleGui1.java
@@ -1,2 +1,26 @@
-public class SimpleGui1 {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleGui1 implements ActionListener {
+    JButton button;
+
+    public static void main(String[] args) {
+        SimpleGui1 gui = new SimpleGui1();
+        gui.go();
+    }
+    public void go(){
+        JFrame frame = new JFrame();
+        button = new JButton("Кликни!");
+        button.addActionListener(this);
+        frame.getContentPane().add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        button.setText("Я нажата!");
+    }
 }
