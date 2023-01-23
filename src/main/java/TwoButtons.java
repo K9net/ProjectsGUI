302893f74468159851a8601c44e74fe1f242adf2
Valeriy V <valera.vasilev.2002@mail.ru>
2023-01-23 09:39:38 +0300

    Two Buttons - change text and change color

---
+++ src/main/java/TwoButtons.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TwoButtons {
+    JFrame frame;
+    JLabel label;
+
+    public static void main(String[] args) {
+        TwoButtons gui = new TwoButtons();
+        gui.go();
+    }
+
+    public void go(){
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton labelButton = new JButton("Изменить текст");
+        labelButton.addActionListener(new LabelListener());
+        JButton colorButton = new JButton("Изменить цвет круга");
+        colorButton.addActionListener(new ColorListener());
+        label = new JLabel("Просто текст");
+        MyDrawPanel drawPanel = new MyDrawPanel();
+        frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
+        frame.getContentPane().add(BorderLayout.EAST, labelButton);
+        frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+        frame.getContentPane().add(BorderLayout.WEST, label);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+    class LabelListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            label.setText("Ты куда-то нажал");
+        }
+    }
+
+    class ColorListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            frame.repaint();
+        }
+    }
+}
