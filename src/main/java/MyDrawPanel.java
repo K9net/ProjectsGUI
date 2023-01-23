9c83749831c62772d03d9b7fcf85d02d6de72ce8
Valeriy V <valera.vasilev.2002@mail.ru>
2023-01-23 09:07:40 +0300

    Change color on click

--- src/main/java/MyDrawPanel.java
+++ src/main/java/MyDrawPanel.java
@@ -4,23 +4,23 @@ import java.awt.*;
 public class MyDrawPanel extends JPanel {
     public void paintComponent(Graphics g) {
         //Рисовка изображения
-        Image image = new ImageIcon("src/main/resources/Image.jpg").getImage();
-        g.drawImage(image, 5, 5, this);
+//        Image image = new ImageIcon("src/main/resources/Image.jpg").getImage();
+//        g.drawImage(image,5,5,this);
 
         //Рисовка фигуры
         //g.fillRect(0,0,this.getWidth(),this.getHeight());
-        int red = (int) (Math.random() * 255);
-        int green = (int) (Math.random() * 255);
-        int blue = (int) (Math.random() * 255);
+            int red = (int) (Math.random()*255);
+            int green = (int) (Math.random()*255);
+            int blue = (int) (Math.random()*255);
 
-        Color randomColor = new Color(red, green, blue);
-        g.setColor(randomColor);
-        g.fillOval(200, 200, 100, 200);
+            Color randomColor = new Color(red,green,blue);
+            g.setColor(randomColor);
+            g.fillOval(50,50,100,100);
 
         //Рисовка фигуры в 2D
-        Graphics2D g2d = (Graphics2D) g;
-        GradientPaint gradient = new GradientPaint(70, 70, Color.blue, 150, 150, Color.orange);
-        g2d.setPaint(gradient);
-        g2d.fillOval(70, 70, 100, 100);
+//        Graphics2D g2d = (Graphics2D) g;
+//        GradientPaint gradient = new GradientPaint(70,70,Color.blue,150,150,Color.orange);
+//        g2d.setPaint(gradient);
+//        g2d.fillOval(70,70,100,100);
     }
 }
---
+++ src/main/java/SimpleGui.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleGui implements ActionListener {
+    JFrame frame;
+
+    public static void main(String[] args) {
+        SimpleGui gui = new SimpleGui();
+        gui.go();
+    }
+    public void go(){
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton button = new JButton("Изменить цвет");
+        button.addActionListener(this);
+        MyDrawPanel panel = new MyDrawPanel();
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        frame.repaint();
+    }
+}
