13ffcf2d910dcfbbd3d24f7d822918bd0ea204a7
Valeriy V <valera.vasilev.2002@mail.ru>
2023-01-23 08:54:00 +0300

    DrawImage and fillRect and Gradient

--- src/main/java/MyDrawPanel.java
+++ src/main/java/MyDrawPanel.java
@@ -5,16 +5,22 @@ public class MyDrawPanel extends JPanel {
     public void paintComponent(Graphics g) {
         //Рисовка изображения
         Image image = new ImageIcon("src/main/resources/Image.jpg").getImage();
-        g.drawImage(image,5,5,this);
+        g.drawImage(image, 5, 5, this);
 
         //Рисовка фигуры
-        g.fillRect(0,0,this.getWidth(),this.getHeight());
-        int red = (int) (Math.random()*255);
-        int green = (int) (Math.random()*255);
-        int blue = (int) (Math.random()*255);
+        //g.fillRect(0,0,this.getWidth(),this.getHeight());
+        int red = (int) (Math.random() * 255);
+        int green = (int) (Math.random() * 255);
+        int blue = (int) (Math.random() * 255);
 
-        Color randomColor = new Color(red,green,blue);
+        Color randomColor = new Color(red, green, blue);
         g.setColor(randomColor);
-        g.fillOval(50,50,100,200);
+        g.fillOval(200, 200, 100, 200);
+
+        //Рисовка фигуры в 2D
+        Graphics2D g2d = (Graphics2D) g;
+        GradientPaint gradient = new GradientPaint(70, 70, Color.blue, 150, 150, Color.orange);
+        g2d.setPaint(gradient);
+        g2d.fillOval(70, 70, 100, 100);
     }
 }
