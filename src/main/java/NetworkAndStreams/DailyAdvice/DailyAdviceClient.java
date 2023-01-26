b8e8de66215195021cffb5950db3394fba215f9b
Valeriy V <valera.vasilev.2002@mail.ru>
2023-01-26 15:20:33 +0300

    DailyAdvice - client and server

---
+++ src/main/java/NetworkAndStreams/DailyAdvice/DailyAdviceClient.java
@@ -0,0 +1,27 @@
+package NetworkAndStreams.DailyAdvice;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class DailyAdviceClient {
+
+    public static void main(String[] args) {
+        DailyAdviceClient client = new DailyAdviceClient();
+        client.go();
+    }
+
+    public void go(){
+        try{
+            Socket socket = new Socket("127.0.0.1", 5000);
+            InputStreamReader is = new InputStreamReader(socket.getInputStream());
+            BufferedReader reader = new BufferedReader(is);
+
+            String advice = reader.readLine();
+            System.out.println("Совет дня: \n" + advice);
+            reader.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/NetworkAndStreams/DailyAdvice/DailyAdviceServer.java
@@ -0,0 +1,35 @@
+package NetworkAndStreams.DailyAdvice;
+
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class DailyAdviceServer {
+
+    public static void main(String[] args) {
+        DailyAdviceServer server = new DailyAdviceServer();
+        server.go();
+    }
+
+    String[] adviceList = {"Ты сегодня стал лучше", "Ешь только правильную еду", "У тебя самый красивый пресс"};
+    public void go(){
+        try{
+            ServerSocket serverSocket = new ServerSocket(5000);
+            while (true){
+                Socket socket = serverSocket.accept();
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+                String advice = getAdvice();
+                printWriter.println(advice);
+                printWriter.close();
+                System.out.println(advice);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public String getAdvice(){
+        int random = (int) (Math.random() * adviceList.length);
+        return adviceList[random];
+    }
+}
