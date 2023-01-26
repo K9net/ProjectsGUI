548042178679ed0cdd3df3acda6346f9901fab0f
Valeriy V <valera.vasilev.2002@mail.ru>
2023-01-26 17:27:40 +0300

    Chat - server and client

--- src/main/java/NetworkAndThreads/SimpleChat/ChatClient.java
+++ src/main/java/NetworkAndThreads/SimpleChat/ChatClient.java
@@ -4,11 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ChatClient {
+    JTextArea incoming;
     JTextField outgoing;
+    BufferedReader reader;
     PrintWriter writer;
     Socket socket;
 
@@ -16,43 +20,73 @@ public class ChatClient {
         new ChatClient().go();
     }
 
-    public void go(){
-        JFrame frame = new JFrame("Простой клиент чата");
+    public void go() {
+        JFrame frame = new JFrame("Чат");
         JPanel miniPanel = new JPanel();
-        outgoing = new JTextField(20);
+        incoming = new JTextArea(15, 30);
+        incoming.setLineWrap(true);
+        incoming.setWrapStyleWord(true);
+        incoming.setEditable(false);
+        JScrollPane sp1 = new JScrollPane(incoming);
+        sp1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        sp1.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        outgoing = new JTextField(30);
         JButton sendButton = new JButton("Отправить");
         sendButton.addActionListener(new SendButtonListener());
+        miniPanel.add(sp1);
         miniPanel.add(outgoing);
         miniPanel.add(sendButton);
         frame.getContentPane().add(BorderLayout.CENTER, miniPanel);
         setUpNetworking();
+
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,200);
+        frame.setSize(500, 400);
         frame.setVisible(true);
     }
 
-    public void setUpNetworking(){
-        try{
+    public void setUpNetworking() {
+        try {
             socket = new Socket("127.0.0.1", 5000);
+            InputStreamReader streamReader = new InputStreamReader(socket.getInputStream());
+            reader = new BufferedReader(streamReader);
             writer = new PrintWriter(socket.getOutputStream());
             System.out.println("Соединение установлено");
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public class SendButtonListener implements ActionListener{
+    public class SendButtonListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            try{
+            try {
                 writer.println(outgoing.getText());
                 writer.flush();
-            } catch (Exception ex){
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
             outgoing.setText("");
             outgoing.requestFocus();
+        }
+    }
+
+    public class IncomingReader implements Runnable {
+
+        @Override
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("Считано " + message);
+                    incoming.append(message + "\n");
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
--- src/main/java/NetworkAndThreads/SimpleChat/ChatServer.java
+++ src/main/java/NetworkAndThreads/SimpleChat/ChatServer.java
@@ -1,5 +1,77 @@
 package NetworkAndThreads.SimpleChat;
 
-public class ChatServer {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class ChatServer {
+    ArrayList clientOutputStreams;
+
+    public class ClientHandler implements Runnable {
+
+        BufferedReader reader;
+        Socket socket;
+
+        public ClientHandler(Socket clientSocket) {
+            try {
+                socket = clientSocket;
+                InputStreamReader isReader = new InputStreamReader(socket.getInputStream());
+                reader = new BufferedReader(isReader);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("Считано " + message);
+                    tellEveryone(message);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new ChatServer().go();
+    }
+
+    public void go() {
+        clientOutputStreams = new ArrayList();
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+                clientOutputStreams.add(writer);
+                Thread t = new Thread(new ClientHandler(clientSocket));
+                t.start();
+                System.out.println("Соединение установлено");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void tellEveryone(String message) {
+        Iterator i = clientOutputStreams.iterator();
+        while (i.hasNext()) {
+            try {
+                PrintWriter writer = (PrintWriter) i.next();
+                writer.println(message);
+                writer.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
